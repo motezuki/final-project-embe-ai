7986e167cd3182d239d13c453af06efb8ab5e913
Fabio Motezuki <fabio.kenji@gmail.com>
2025-02-26 13:08:38 -0500

    Add function emotion_detector

---
+++ emotion_detection.py
@@ -0,0 +1,8 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    my_obj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, headers=headers, json=my_obj, timeout=30)
+    return response.text
